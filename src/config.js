f40df6401f303253ce121a453954077b5f72e63e
riska sholikha <riskalailatussholikha05@gmail.com>
2023-02-03 17:00:31 +0700

    Login Google

--- src/config.js
+++ src/config.js
@@ -8,14 +8,15 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA405H8rnTbL8-t0l__Qmp7TZzk3onurcc",
-  authDomain: "login-riska.firebaseapp.com",
-  projectId: "login-riska",
-  storageBucket: "login-riska.appspot.com",
-  messagingSenderId: "4562150750",
-  appId: "1:4562150750:web:65e45f352027adc887dbcf",
-  measurementId: "G-Z1MFHCDXMR"
-};
+    apiKey: "AIzaSyBhYd27jyyfaag3-2BUpfJ-RJjgutXDRjg",
+    authDomain: "login-85b9f.firebaseapp.com",
+    projectId: "login-85b9f",
+    storageBucket: "login-85b9f.appspot.com",
+    messagingSenderId: "408111748523",
+    appId: "1:408111748523:web:8d2e268efd2956a7d52eee",
+    measurementId: "G-Z8Q1R2E5GG"
+  };
+  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
